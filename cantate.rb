6c522470f3b51b883f5dd892be6eebc704263d2f
George Toothman <george.toothman@gmail.com>
2019-09-02 00:49:38 -0400

    add proper spacing and indentation to file

--- cantate.rb
+++ cantate.rb
@@ -1,65 +1,66 @@
 class Cantate
-	attr_reader :twitter, :paymail, :client, :twetch_app
+  attr_reader :twitter, :paymail, :client, :twetch_app
 
-	def initialize(twitter, paymail, client, twetch_app)
-    	@twitter = twitter
-    	@paymail = paymail
-    	@client = client
-    	@twetch_app = twetch_app
+  def initialize(twitter, paymail, client, twetch_app)
+    @twitter = twitter
+    @paymail = paymail
+    @client = client
+    @twetch_app = twetch_app
+  end
+  
+  author_array = []
+
+  paymail = [ paymail: ["drovers"], 
+    twttr: ["@cantatedotco"], 
+    baemail: ["https://baemail.me", "drovers@moneybutton.com"]
+  ]
+
+  author_array << ["@toothman", "#applemusic", "$bsv"]
+  client = [ client_twttr: ["@applemusic", "@apple"], client_tags: ["#applemusic", "#apple"]]
+
+  twitter = [ related_apps: ["@twetchapp", "@twitter", "@paymailhandles", "@money_button"], hash_tags: ["$bsv", "#retweet"] ]
+
+  twetch_app = [ twetch_twttr: "@twetchapp", 
+    ultra_tags: ["@1133", "@2459", "@852"],
+    twetch_ids: ["u/1133", "u/2459", "u/852"] 
+  ]
+
+  def self.post_tags(twitter)
+    twitter.each do |item|
+      puts "#{item}"
     end
-    author_array = []
+  end
 
-    paymail = [ paymail: ["applemusic"], 
-    			contact_twttr: ["@cantatedotco"], 
-    			contact_paymail: ["http://moneybutton.email", "drovers@moneybutton.com"]
-    			]
+  def self.client_details(client)
+    client.each do |item|
+      puts "#{item}"
+    end
+  end
 
-    author_array << ["@toothman", "#applemusic", "$bsv"]
-    client = [ client_twttr: ["@applemusic", "@apple"], client_tags: ["#applemusic", "#apple"]]
+  def self.author_details(author_array)
+    author_array.each do |item|
+      puts "#{item}"
+    end
+  end
 
-    twitter = [ related_apps: ["@twetchapp", "@twitter", "@paymailhandles", "@money_button"], hash_tags: ["$bsv", "#retweet"] ]
+  def self.advertise_paymail(paymail)
+    paymail.each do |details|
+      puts "#{details}"
+    end
+  end
 
-    twetch_app = [ twetch_twttr: "@twetchapp", 
-    	ultra_tags: ["@1133", "@2459", "@852"],
-    	twetch_ids: ["u/1133", "u/2459", "u/852"] 
-    ]
-
-
-    def self.post_tags(twitter)
-		twitter.each do |item|
-			puts "#{item}"
-		end
-	end
-
-	def self.client_details(client)
-		client.each do |item|
-			puts "#{item}"
-		end
-	end
-
-	def self.author_details(author_array)
-		author_array.each do |item|
-			puts "#{item}"
-		end
-	end
-
-	def self.advertise_paymail(paymail)
-		paymail.each do |details|
-			puts "#{details}"
-		end
-	end
-
-	def self.twetch_metatags(twetch_app)
-		twetch_app.each do |item|
-			puts "#{item}"
-		end
-	end
-
-	advertise_paymail(paymail)
-	puts "-----------------------------------------"
-	# author_details(author_array)
-	client_details(client)
-	post_tags(twitter)
-	puts "-----------------------------------------"
-	twetch_metatags(twetch_app)
+  def self.twetch_metatags(twetch_app)
+    twetch_app.each do |item|
+      puts "#{item}"
+    end
+  end
 end
+
+
+advertise_paymail(paymail)
+puts "-----------------------------------------"
+# author_details(author_array)
+client_details(client)
+post_tags(twitter)
+puts "-----------------------------------------"
+twetch_metatags(twetch_app)
